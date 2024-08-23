48506ac1da0dcb16868cbcb55b0c63cd6aeb99f6
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-23 13:11:04 +0530

    to check whether num is positive or negative

---
+++ C/num_positive_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+     int num1;
+     printf("enter the number:");
+     scanf("%d",&num1);
+     if(num1>0)
+     {
+         printf("The number is positive");
+     }
+     else
+     {
+         printf("The number is negative");
+     }
+
+    return 0;
+}
