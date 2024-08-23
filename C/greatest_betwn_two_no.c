a6720efc7d1b316e3208c39823ae7accc06ef9c3
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-23 13:09:34 +0530

    greatest betwenn two numbers

---
+++ C/greatest_betwn_two_no.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int num1,num2;
+  printf("Enter any two numbers : ");
+  scanf("%d%d",&num1,&num2);
+   
+   if(num1>num2)
+   {
+       printf("%d is greater than %d",num1,num2);
+   }
+   else
+   {
+       printf("%d is greater than %d",num2,num1);
+   }
+    return 0;
+}
