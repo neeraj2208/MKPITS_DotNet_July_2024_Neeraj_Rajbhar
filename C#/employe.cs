49e1e0ba9e4407a6c747d30011a521fc3a672789
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-26 13:22:36 +0530

    program on employee name with id

---
+++ C#/employe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array.empnumb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string [] Employename = {"Neeraj","Suraj"};
+            int[] employeID = { 1, 2 };
+
+            Console.Write("Employee Name\t\t\t Employee ID");
+            Console.Write("                               ");
+
+            for(int i = 0; i < employeID.Length; i++) 
+            { 
+                Console.WriteLine(employeId[i])+"\t"+ Employename[i]);
+            }     
+            Console.ReadLine();
+        }
+    }
+}
