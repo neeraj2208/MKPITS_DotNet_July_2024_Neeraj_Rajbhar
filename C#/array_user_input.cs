16fcce622029d39f7216d759468b6f99f99c4c15
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-26 16:57:01 +0530

    print array using array input

---
+++ C#/array_user_input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter lements of array:");
+            int[] arrElemets = new int[5];
+
+            for (int position = 0; position < 5; position++)
+            {
+                arrElemets[position] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("elements in array area: ");
+            for (int position = 0; position < 5; position++)
+            {
+                Console.WriteLine(arrElemets[position]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
