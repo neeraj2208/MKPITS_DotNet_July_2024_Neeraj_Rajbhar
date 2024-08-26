79c01d7b1382cee3144673ea9cbe3198e7a0c1ef
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-26 10:55:22 +0530

    find array through position

---
+++ C#/array_position.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] age = { 10, 20, 30, 40, 50 };
+            Console.Write("Array :" + age[2]);
+            Console.Read();
+        }
+       
+    }
+    
+}
