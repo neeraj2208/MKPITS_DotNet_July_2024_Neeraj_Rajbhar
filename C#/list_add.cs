58e385ad990423fb5d235713aa59fc6e6d0b0fd3
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-26 17:53:01 +0530

    print the list with add function

---
+++ C#/list_add.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace arraylist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr1=new ArrayList();
+            arr1.Add(1);
+            arr1.Add("Neeraj ");
+            arr1.Add(1.8);
+            arr1.Add(18);
+
+            Console.WriteLine(arr1.Capacity);
+            for (int index = 0; index < arr1.Capacity; index++)
+            {
+                Console.WriteLine(arr1[index]);
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
