4adaa0e99a2cd73d7edf42c57d5ea22a7050806c
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-26 17:11:38 +0530

    sum of array upto 10 digit

---
+++ C#/sum_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] num = new int[10];
+            Console.WriteLine("the numbers are:");
+            for (int position = 0; position < 10; position++)
+            {
+                num[position] =  position + 1  ;
+            }
+           
+            int sum = 0;
+            for (int position = 0; position < num.Length; position++)
+            {
+
+                sum += num[position];
+            }
+            Console.Write(sum);
+            Console.ReadLine();
+        }
+    }
+}
