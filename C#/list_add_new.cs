da8a70433ea792c93b338f1bcae16bd92f55903a
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-26 17:54:06 +0530

    print list with add function

---
+++ C#/list_add_new.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace arraylist1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr= new ArrayList();
+            arr.Add("8-1=7");
+            arr.Add("MS Dhoni");
+            arr.Add("Back to pavilion");
+            
+
+            Console.WriteLine("Initial Count of array List:" + arr.Count);
+            for(int iterate=0;iterate<arr.Count; iterate++)
+            {
+                Console.WriteLine(arr[iterate]);
+            }
+            arr.Add("greatest captain");
+            arr.Add("of the era");
+            Console.WriteLine("------New ArrayList-------");
+            for(int index=0;index<arr.Count;index++)
+                { Console.WriteLine(arr[index]); }
+            Console.ReadLine();
+        }
+        
+    }
+}
