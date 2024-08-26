7a0cf197ffa41df0c71e710c74fbbbc7e4e25126
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-26 16:53:25 +0530

    Sum of position of array

---
+++ C#/sum_position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3 , 4, 5, 6, 7, 8, 9};
+            int Sum = 0;
+            Sum = num[0] + num[4];
+            Console.WriteLine(Sum);
+            Console.Read();
+        }
+    }
+}
