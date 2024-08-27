7fbb293d4901190bcf4d50ab06867067246b6347
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-27 16:48:06 +0530

    program based on multiple inheritance

---
+++ C#/OPP/multiple_inheritance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_multiple
+{
+    class Base
+    {
+        public void fnBase()
+        {
+
+        }
+
+    }
+    interface infBase
+    {
+        void fnInterface();
+        void myInterface();
+    }
+
+    class Derived :Base,infBase
+    {
+        public void fnInterface()
+        {
+
+        }
+
+        public void myInterface()
+
+        {
+            Console.WriteLine("neeraj");
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived d = new Derived();
+            d.myInterface();
+            Console.Read();
+        }
+    }
+
+}
