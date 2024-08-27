536f9844a14c5abff18a72be0fd4ff3aaea9ec0a
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-27 17:46:36 +0530

    to check whether number is positive or negative

---
+++ C#/Console/positive_negative.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter the First number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            if(num1>0)
+            {
+                Console.Write("The Number is positive");
+            }
+            else
+            {
+                Console.Write("The Number is negative");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
