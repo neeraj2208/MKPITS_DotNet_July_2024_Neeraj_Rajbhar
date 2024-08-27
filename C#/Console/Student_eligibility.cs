3b69624c9c1d9ea778bb320984effd60a9e4f8be
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-27 17:37:51 +0530

    student eligible for admission or not

---
+++ C#/Console/Student_eligibility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total, totmp;
+
+            Console.Write("Enter the marks of Physics: ");
+            int phy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the marks of Maths: ");
+            int math = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the marks of Chemistry:");
+            int chem = Convert.ToInt32(Console.ReadLine());
+
+            total = math + phy + chem;
+            totmp = math + phy;
+
+            if (total >= 0 && totmp >= 0)
+            {
+                Console.Write("the student is eligible for admission.");
+
+            }
+            else
+            {
+                Console.Write("The student is not eligible for admission.");
+            }
+            Console.Read();
+        }
+    }
+}
+
+
+      
+     
