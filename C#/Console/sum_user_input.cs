666c9e79a88e9b5a40c35eb0f013f2c7365c2049
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-27 17:42:18 +0530

    sum by user input

---
+++ C#/Console/sum_user_input.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum=0;
+            Console.Write("Enter the Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                sum=sum+i; 
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
