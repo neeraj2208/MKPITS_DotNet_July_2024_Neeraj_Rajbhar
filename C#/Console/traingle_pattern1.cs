fa04756bc07cf818380d010d0b49e6d621f99965
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 16:17:04 +0530

    right angle triangle with  * pattern

---
+++ C#/Console/traingle_pattern1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace right_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <= 5; counter++)
+            {
+                int x = 1;
+                for (int number = 1; number <= counter; number++) 
+                {
+                    Console.Write("*");
+                    x++;
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+       
+    }
+}
