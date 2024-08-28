9b59f69f76771b71b34da175b80c391f1480b4e9
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 17:05:26 +0530

    printing static value of multidimensional array

---
+++ C#/array/multiDimensional_static.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multidimensinolarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] Name = {{"A","B","C","D","E","F"},
+                              {"G","H","I","J","K","L"},
+                              {"M","N","O","P","Q","R"},
+                              {"S","T","U","V","W","X"} };
+
+            for (int row = 0; row < 4; row++) 
+            {
+                for (int col = 0; col < 6; col++)
+                {
+                    Console.Write(Name[row,col]+"\t");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    } }
