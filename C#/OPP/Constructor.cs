26a03619b8ceab026107d9db90d1edeef1ecf8e8
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 13:18:27 +0530

    how to use constructor

---
+++ C#/OPP/Constructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Base
+    {
+        public void fnBase()
+        {
+            Console.WriteLine("I am  a base class");
+        }
+        public Base()
+        {
+            Console.WriteLine("I am a constructor");
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Base @base = new Base(); //Constructor invoked
+            @base.fnBase();
+            Console.Read();
+        }// acts like destructors
+    }
+}
