dd0d34ae2c81cb6e5a4c318e12dd0b72439d4546
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 10:44:29 +0530

    to check the height of person

---
+++ C#/Console/Person_height.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            Console.Write("Enter the Height of Person in centimeters :");
+            height=Convert.ToInt32(Console.ReadLine());
+
+            if (height > 0 && height <= 100)
+            {
+                Console.WriteLine("The height of person is too short");
+            }
+            else if (height >100 && height<=150)
+            {
+                Console.WriteLine("The person is Dwarf");
+            }
+            else
+            {
+                Console.WriteLine("The person is heighted");
+            }
+            Console.ReadLine();
+        }
+    }
+}
