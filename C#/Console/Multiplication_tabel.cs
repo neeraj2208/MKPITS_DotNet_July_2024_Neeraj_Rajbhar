39de879b20a0aad7de20afa3baa9fe180a83b5aa
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 15:48:03 +0530

    multiplication tabel from 1 to 10

---
+++ C#/Console/Multiplication_tabel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <= 10; counter++)
+            {
+               
+                for (int number = 1; number <= 10; number++)
+                {
+                    Console.Write(counter * number);
+                    Console.Write("\t");
+                }
+                Console.WriteLine();
+                
+            }
+            Console.Read();
+        }
+    }
+}
