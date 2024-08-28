423987609b7dbf526d6257a5e59578f2be4899e1
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 10:31:46 +0530

    to check whether the enter year is leap year or not

---
+++ C#/Console/Leap_year.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the year: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num%4 == 0)
+            {
+                Console.WriteLine(num + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("These is not a leap year");
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
