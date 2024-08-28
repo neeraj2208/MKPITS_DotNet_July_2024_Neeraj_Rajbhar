335f28b414fa17ea96c253066f02543ca9535e23
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 10:42:43 +0530

    to check whether the number lies

---
+++ C#/Console/condition_on_number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+            Console.Write("Enter the value of M: ");
+            m=Convert.ToInt32(Console.ReadLine());
+
+            if (m > 0)
+            {
+                Console.WriteLine("the value of n is 1");
+            }
+            else if (m == 0)
+            {
+                Console.WriteLine("The value of n is 0");
+            }
+            else
+            {
+                Console.WriteLine("The value of n is -1");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
