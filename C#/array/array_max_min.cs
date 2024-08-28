05fbe0978bc8dfdd2a10323181ba27a3f67a3d2d
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 17:30:32 +0530

    find max and min in array

---
+++ C#/array/array_max_min.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max_min_array1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2,3,7,1,2,9,5,6 };
+
+            int max = 0;
+            int min =array [0];
+
+            for (int iterate = 1; iterate < array.Length; iterate++)
+            {
+                if (array[iterate] > max)
+                {
+                    max = array[iterate];
+                }
+                else if (array[iterate] < min)
+                {
+                    min = array[iterate];
+                }
+            }
+
+            Console.WriteLine("The Maximum Element is: " + max);
+            Console.WriteLine("The Minimum element is: " + min);
+            Console.ReadLine();   
+        }
+    }
+}
