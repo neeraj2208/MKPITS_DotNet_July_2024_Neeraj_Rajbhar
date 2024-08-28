2b8b5035287e56e2e490567dd5f07fbbcff0d962
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 10:39:44 +0530

    to check whether the candidate eligible for vote or not

---
+++ C#/Console/voting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.Write("Enter the age of Candidate:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("HE/SHE is eligible for voting");
+
+            }
+            else
+            {
+                Console.WriteLine("HE/SHE is not eligible for voting");
+            }
+            Console.ReadLine();
+        }
+    }
+}
