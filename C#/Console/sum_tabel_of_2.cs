9a2e3f72929f16870b11cc019f82e7138edbe32c
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 15:36:03 +0530

    sum of tabel of 2

---
+++ C#/Console/sum_tabel_of_2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_even_natural
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ven=0;
+            int sum = 0;
+            for (int num = 0; num < 10; num++)
+            {
+                ven = num * 2;
+                Console.WriteLine(ven);
+                sum = sum + ven;
+            }
+            Console.WriteLine("sum is "+sum);
+            Console.Read();
+        }
+    }
+}
