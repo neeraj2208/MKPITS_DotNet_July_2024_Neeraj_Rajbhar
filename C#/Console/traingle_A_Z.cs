554578a6651c2a7c5e1c813f33703e0fa6d7bfa4
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 16:12:57 +0530

    triangle pattern from A to Z

---
+++ C#/Console/traingle_A_Z.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyramidA_Z
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            char currentchar= 'A';
+
+            Console.Write("Enter the length of height: ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(currentchar + " ");
+
+                    if (currentchar == 'Z')
+                    {
+                        currentchar = 'A';
+                    }
+                    else
+                    {
+                        currentchar++;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
