d003a3813d57d61f215811631f4491b7e8d653f5
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-28 10:55:25 +0530

    converting dollar to INR

---
+++ C#/Console/dollar_to_INR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dollar;
+            float INR;
+            Console.Write("Enter the amount of dollar: ");
+            dollar=Convert.ToInt32(Console.ReadLine());
+
+            INR = 83 * dollar;
+            Console.Write("The Inr Amount is: "+INR); 
+            Console.ReadLine();
+
+
+        }
+    }
+}
