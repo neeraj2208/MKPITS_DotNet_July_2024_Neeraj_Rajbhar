f60bb891d965963e582715fc7d40fd90fe58a1e2
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-29 16:58:01 +0530

    list program using for each loop

---
+++ C#/List/List_forEach.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> marks = new List<int>() { 76, 99, 87, 70, 80, 85 };
+            List<string> Subjects = new List<string>() { "English", "Maths", "Hindi", "Marathi", "Sanskrit", "History" };
+            Subjects.Add("Chemistry");
+            foreach (int mark in marks)
+            {
+                Console.WriteLine(mark);
+
+            }
+            Subjects.Remove("English");
+            Subjects.Insert(0,"Biology");
+            foreach (string sub in Subjects)
+            {
+                Console.WriteLine(sub);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
