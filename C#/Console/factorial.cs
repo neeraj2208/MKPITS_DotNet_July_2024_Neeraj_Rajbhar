473f3d840394d72de833ecbe929877bf6bf10165
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-29 16:47:17 +0530

    jagged array by user input

---
+++ C#/Console/factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for(int num = 1; num <= n; num++)
+            {
+                fact= fact*num;      
+            }
+            Console.WriteLine("factorial of "+n+" is "+fact);
+
+            Console.Read();
+                
+                
+        }
+    }
+}
---
+++ C#/array/jagged_user.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrjagguser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] ladder = new int[3][];
+            ladder[0]= new int[3];
+            ladder[1]= new int[4];
+            ladder[2]= new int[2];
+            foreach(int[] row in ladder)
+            {
+                foreach(int col in row)
+                {
+                    Console.Write(col);
+                }
+             Console.WriteLine();   
+            }
+            Console.ReadLine();
+        }
+    }
+}
