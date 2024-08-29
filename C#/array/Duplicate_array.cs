0ae8c6d0c0e13cb2a3d0e30e945aa124ea61866c
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-29 17:03:14 +0530

    to check the duplicate elements in array

---
+++ C#/array/Duplicate_array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duplicate_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements in array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("-----------Enter the elements in array--------------");
+            for (int iterate = 0; iterate < n; iterate++)
+            {
+                arr[iterate] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Duplicate elements in array:");
+            for (int iterate1 = 0; iterate1 < arr.Length; iterate1++)
+            {
+                for (int iterate2 = 0; iterate2 < iterate1; iterate2++)
+                {
+
+                    if (arr[iterate1] == arr[iterate2])
+                    {
+                        Console.WriteLine(arr[iterate1]);
+                        break;
+                    }
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
