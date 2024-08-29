e9754264cd33ef867d7281093215a2235b9ee161
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-08-29 16:13:24 +0530

    printing jagged array

---
+++ C#/array/jagged.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrrayjagged
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] ladder = new int[3][];
+            ladder[0] = new int[] { 10, 20 };
+            ladder[1] = new int[] { 30, 40, 50 };
+            ladder[2] = new int[] { 60, 70, 80, 90 };
+
+            for (int row = 0; row < 3; row++)
+            {
+                
+                for (int col= 0;col< ladder[row].Length;col++)
+                {
+                    Console.Write(ladder[row][col]+ "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+        
+    
