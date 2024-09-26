46b141399f01e08e3f8b9992865191009247b18d
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-09-26 17:13:23 +0530

    print even num from 1 to 100

---
+++ CPP/1 to 100 even num.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int counter;
+	for(counter=1;counter<=100;counter++)
+	{
+		if(counter%2==0)
+		{
+			cout<<counter<<endl;
+		}
+	}
+	return 0;
+}
