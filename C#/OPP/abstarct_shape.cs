257214d767e2c31d86f9d4154b8fdbb7fe967663
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-09-26 17:05:47 +0530

    abstract code

---
+++ C#/OPP/abstarct_shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Shape
+{
+    abstract class Shape
+    {
+        public abstract void draw();
+
+    }
+    class Rectangle:Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Rectangle drawing...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s = new Rectangle();
+            s.draw();
+            Console.ReadKey();
+
+        }
+    }
+}
