4a6441fdafedb24db086741d8325d8cb839b9915
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-09 12:59:10 +0530

    insert query

---
+++ SQL/Command/Insert/insert2.sql
@@ -0,0 +1,4 @@
+Insert into Customer (FirstName,LastName,City,Country,Phone)values('Neeraj','Rajbhar','Nagpur','INDIA',8483831723);
+Insert into Customer (FirstName,LastName,City)values('Neelesh','Mishra','Lahore');
+Insert into Customer (FirstName,LastName,City,Country,Phone)values('Niraj','Raj','Nagr','INDIA',848383143),('Suraj','Rajbhar','Nagpur','INDIA',8483831723),('dheraj','Rajbhar','Nagpur','INDIA',9833831723),('Neeraj','Rajbhar','kanpur','INDIA',843353431723),('Neeraj','sinhg','Nagpur','INDIA',8483831723);
+Insert into Customer (City,Country)values('Nagpur','INDIA');
