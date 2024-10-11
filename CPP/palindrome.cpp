0052847859a758632ed44b07ae6079eacab7b10a
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-11 12:36:14 +0530

    to check whether the number is palindrome or not

---
+++ CPP/palindrome.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string word;
+    bool isPalindrome = true;
+
+    cout << "Enter a word: ";
+    cin >> word;
+
+    int length = word.length();
+
+    for (int i = 0; i < length / 2; i++) 
+    {
+        if (word[i] != word[length - 1 - i])
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome) 
+    {
+        cout << word << " is a palindrome." << endl;
+    } 
+    else
+    {
+        cout << word << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
