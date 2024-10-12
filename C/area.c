ff96e3f878f099eb84874e883a0089a64992cc45
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-13 00:06:20 +0530

    area

---
+++ C/area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	int area,length,breadth;
+	printf("Enter length");
+	scanf("%d",&length);
+	printf("Enter breadth");
+	scanf("%d",&breadth);
+	area=length*breadth;
+	printf("area=%d",area);
+}
