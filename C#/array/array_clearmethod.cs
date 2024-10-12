2129bd5fcae0993f4f35c52654b23bed5bc82fb8
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-13 00:11:07 +0530

    array clear method

---
+++ C#/array/array_clearmethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayClearMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Array.Clear(arr, 0, 5);
+            for(int cnt=0;cnt<arr.Length;cnt++)
+            {
+                Console.WriteLine(arr[cnt]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
