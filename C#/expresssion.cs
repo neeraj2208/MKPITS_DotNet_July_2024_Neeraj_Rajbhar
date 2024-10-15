cd158ddeb749ae9566ad9e61e5cd0d2ff8818cb0
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-15 23:35:50 +0530

    expression

---
+++ C#/expresssion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace expression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, sum;
+            Console.Write("Eenter x :");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter y :");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter z :");
+            z = Convert.ToInt32(Console.ReadLine());
+            sum = (x + y)*z;
+            Console.Write("Total is " + sum);
+            sum = x * y + y * z;
+            Console.Write("Total is " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
