d9d44f235686a4e491bada0736b9fd2d4c0c1e7d
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-21 15:53:09 +0530

    length of a string

---
+++ JavaScript/length.js
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<p>The length of the string is:</p>
+<script>
+let text = "Neeraj Rajbhar";
+document.getElementById("demo").innerHTML = text.length;
+</script>
+
+</body>
+</html>
