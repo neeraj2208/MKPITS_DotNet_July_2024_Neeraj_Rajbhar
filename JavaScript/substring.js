b0cff72842e50478f3b02e7fabd5572348291725
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-21 16:47:51 +0530

    use of substring in js

---
+++ JavaScript/substring.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="demo"></p>
+<script>
+let str = "Apple, Banana, Kiwi";
+document.getElementById("demo").innerHTML = str.substring(0,5);
+</script>
+</body>
+</html>
