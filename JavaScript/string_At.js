466f5d3a8b8c5191629af67e741dbb7ab1cb24b6
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-21 16:38:22 +0530

    use of string At in javascript

---
+++ JavaScript/string_At.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="11"></p>
+
+<script>
+const name = "Neeraj Rajbhar";
+let letter = name.at(2);
+
+document.getElementById("11").innerHTML = letter;
+</script>
+
+</body>
+</html>
