302478d6217fa5090f6337e5daafa9acade6e7d9
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-21 14:30:17 +0530

    increment in javaScript

---
+++ JavaScript/increment.js
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>JavaScript Operators</h1>
+<p id="demo1"></p>
+<p id="demo2"></p>
+<script>
+var y = 5;
+var x = ++y;
+document.getElementById("demo1").innerHTML = y;
+document.getElementById("demo2").innerHTML = x;
+</script>
+</body>
+</html>
