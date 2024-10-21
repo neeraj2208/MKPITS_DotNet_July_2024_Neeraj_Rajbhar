ec8f9084d0227b7a9e6e38a40f171f6727899c30
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-21 16:45:03 +0530

    use of slice method in js

---
+++ JavaScript/slice_method.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="11"></p>
+<script>
+let text = "Apple, Banana, Kiwi";
+let letter = text.slice(7,13);
+document.getElementById("11").innerHTML = letter; 
+</script>
+</body>
+</html>
+
+
