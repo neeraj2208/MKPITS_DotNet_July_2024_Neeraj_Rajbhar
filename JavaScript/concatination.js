85a9d4b782f813e1924e8afe2b9246fa6a23898e
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-21 17:45:04 +0530

    concat in js

---
+++ JavaScript/concatination.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="11"></p>
+<script>
+let text1 = "Hello";
+let text2 = "Neeraj";
+let text3 = text1.concat(" ",text2);
+document.getElementById("11").innerHTML = text3;
+</script>
+</body>
+</html>
