1b59b4a3c04c129d5fb0bf5c5549c7a16f2d3124
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 15:52:59 +0530

    The unshift() method adds new elements to the beginning of an array

---
+++ JavaScript/unshift.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.write(fruits + "<br>"); 
+fruits.unshift("Lemon");
+document.write(fruits);
+</script>
+
+</body>
+</html>
