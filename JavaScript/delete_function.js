9a0066580e899a1e38294e96a890c7c7f227e606
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 16:03:16 +0530

    delete function in js

---
+++ JavaScript/delete_function.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+document.write(
+"The first fruit is: " + fruits[0]+"</br>");
+delete fruits[0] ;
+document.write(
+"The first fruit is: " + fruits[0]);
+</script>
+
+</body>
+</html>
