168cdc2920ccc95d078d99acd3e92215a02f8707
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 13:28:50 +0530

    switch case vowels

---
+++ JavaScript/vowels.js
@@ -0,0 +1,18 @@
+<html>
+    <script>
+        var ch=prompt("Enter character");
+        switch(ch)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                alert("Vowels");
+                break;
+            default:
+                alert("Not vowels");
+                break;
+        }
+    </script>
+</html>
