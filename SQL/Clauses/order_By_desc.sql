788c924f155e5a669bb11491221e42433e3ec930
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 17:42:00 +0530

    order by using descending

---
+++ SQL/Clauses/order_By_desc.sql
@@ -0,0 +1,4 @@
+select * from customers ORDER BY Name desc; 
+select * from customers ORDER BY address desc; 
+select * from customers ORDER BY contact desc; 
+select * from customers ORDER BY position desc;  
