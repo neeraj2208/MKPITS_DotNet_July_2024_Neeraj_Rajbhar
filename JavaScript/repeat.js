e19a7a54b8ad59bcc460227053d496b6f47b7f8a
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 10:45:30 +0530

    use of repeat function in js

---
+++ JavaScript/repeat.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="11"></p>
+<script>
+let text = "Hello Neeraj ";
+let result = text.repeat(4);
+document.getElementById("11").innerHTML = result;
+</script>
+</body>
+</html>
