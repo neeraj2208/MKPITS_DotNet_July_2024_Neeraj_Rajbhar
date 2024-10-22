4a2b8b07fe1d93cb0e871ff68d3d8248cdb9deb6
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 12:07:22 +0530

    use of search function jn js

---
+++ JavaScript/search.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>The position of the first occurrence of "locate" is:</p>
+<p id="11"></p>
+<script>
+let text = "Please locate where 'locate' occurs!";
+let index = text.search("locate");
+document.getElementById("11").innerHTML = index; 
+</script>
+</body>
+</html>
+
