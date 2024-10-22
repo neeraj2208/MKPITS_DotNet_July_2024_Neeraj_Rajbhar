4ce85832850808046206b478a3e8cd2bac82c049
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 15:42:41 +0530

    The pop() method removes the last element from an array

---
+++ JavaScript/pop.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.write(fruits);
+fruits.pop();
+document.write(fruits);
+</script>
+
+</body>
+</html>
