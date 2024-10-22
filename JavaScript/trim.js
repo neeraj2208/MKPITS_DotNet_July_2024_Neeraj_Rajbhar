73e5d9f3b678bca8f6c0104d288f15d2b72aff4a
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 10:31:09 +0530

    use of trim function in js

---
+++ JavaScript/trim.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="11"></p>
+<script>
+let text1 = "     Hello Neeraj     ";
+let text2 = text1.trimEnd();
+document.getElementById("11").innerHTML =text2;
+</script>
+</body>
+</html>
