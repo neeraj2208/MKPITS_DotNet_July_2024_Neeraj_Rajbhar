4e022f3e6ce7f360660c9f0e1d187b1ff76f6524
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 13:04:38 +0530

    variabel

---
+++ JavaScript/variabel.js
@@ -0,0 +1,8 @@
+<html>
+    <script>
+        var name="Neeraj";
+        var last="Rajbhar";
+        document.write("Name is "+name+"<br>");
+        document.write("last is "+num);
+    </script>
+</html>
