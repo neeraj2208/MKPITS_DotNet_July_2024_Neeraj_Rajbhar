c32dca6bf318503694b5aaeb5f5ba71745bb98e8
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 10:36:39 +0530

    use of pad end in js

---
+++ JavaScript/padEnd.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="11"></p>
+<script>
+let text = "5";
+text = text.padEnd(4,"0");
+document.getElementById("11").innerHTML = text;
+</script>
+</body>
+</html>
