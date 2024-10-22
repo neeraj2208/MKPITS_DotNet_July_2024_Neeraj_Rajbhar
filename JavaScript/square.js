582a039f48f30a9d6ecb0ce96137fab22c4cde91
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 13:30:27 +0530

    square

---
+++ JavaScript/square.js
@@ -0,0 +1,9 @@
+<html>
+    <script>
+        var num,square;
+        num=prompt("Enter num");
+        square=num*num;
+        alert("Square is "+square);
+        document.write("Square is "+square);
+    </script>
+</html>
