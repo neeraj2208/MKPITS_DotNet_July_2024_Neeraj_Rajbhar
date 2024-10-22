4d287253fe2e0cd97a396e4c95ec60b79e6fc0f1
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 11:26:10 +0530

    use of index of in js

---
+++ JavaScript/indexOf.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="11"></p>
+<script>
+let text = "Please locate where 'locate' occurs!";
+let index = text.indexOf("locate");
+document.getElementById("11").innerHTML = index; 
+</script>
+</body>
+</html>
