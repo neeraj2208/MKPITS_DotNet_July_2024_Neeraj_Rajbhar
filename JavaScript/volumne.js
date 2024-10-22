9181b8e8ea4d516df02b5a58a81153fe571a146f
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-22 12:56:10 +0530

    volumne in javascript

---
+++ JavaScript/volumne.js
@@ -0,0 +1,13 @@
+<html>
+    <script>
+        var length,breadth,height,volume;
+
+        length=prompt("Enter length:");
+        breadth=prompt("Enter breadth:");
+        height=prompt("Enter height:");
+
+        volume=length*breadth*height;
+        document.write("Volume is "+volume);
+        alert("volume is "+volume);
+    </script>
+</html>
