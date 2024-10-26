2f2f7cfd5920fc67ffcee54522de59adda475f25
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-26 23:49:40 +0530

    multiplication tabe of a five

---
+++ JavaScript/multiplication_table.js
@@ -0,0 +1,7 @@
+function multiplicationTable() {
+    console.log("Print Multiplication Table of a Number (5)");
+    var number = 5;
+    for (var i = 1; i <= 10; i++) {
+        console.log(number + " x " + i + " = " + (number * i)); 
+    }
+}
