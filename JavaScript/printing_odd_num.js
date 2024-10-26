656219ce9ff5bee081156e1d862ad38d4c81ca6a
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-26 23:46:24 +0530

    printing odd number

---
+++ JavaScript/printing_odd_num.js
@@ -0,0 +1,8 @@
+function printOddNumbers() {
+    console.log("Print Odd Numbers from 1 to 20");
+    for (var i = 1; i <= 20; i++) {
+        if (i % 2 !== 0) {
+            console.log(i); 
+        }
+    }
+}
