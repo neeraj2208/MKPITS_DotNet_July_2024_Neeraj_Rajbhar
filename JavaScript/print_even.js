376c78d573568ac0162e0aa30bb1775db8c49169
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-10-26 23:45:03 +0530

    printing even number from 1 to 10

---
+++ JavaScript/print_even.js
@@ -0,0 +1,8 @@
+function printEvenNumbers() {
+    console.log("Print Even Numbers from 1 to 20");
+    for (var i = 1; i <= 20; i++) {
+        if (i % 2 === 0) {
+            console.log(i); 
+        }
+    }
+}
