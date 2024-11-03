671ba9a208d8843b9b3959f96a37b7303dddc7a0
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-11-03 18:59:46 +0530

    picture box

---
+++ C#/Window_Tool/picture_Box.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Box_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string imgname = comboBox1.Text;
+            for(int i=0;i<100;i++)
+            {
+                progressBar1.Value = i;
+            }
+            pictureBox2.Image = System.Drawing.Image.FromFile(imgname);
+
+        }
+    }
+}
