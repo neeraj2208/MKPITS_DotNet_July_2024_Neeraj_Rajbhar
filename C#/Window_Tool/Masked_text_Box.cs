17ec1c445c424366ee0b27631dbddccf1179ad69
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-11-03 19:02:17 +0530

    masked text box

---
+++ C#/Window_Tool/Masked_text_Box.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Masked_Test_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = maskedTextBox1.Text;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+    }
+}
