7142ac3a1ebfeeccd2a65f81c81987b042e8c52f
Neeraj Rajbhar <rajbharneeraj2002@gmail.com>
2024-11-03 19:00:51 +0530

    image list

---
+++ C#/Window_Tool/Image_list.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "Account")
+            {
+                MessageBox.Show("You select account");
+            }
+            
+               
+        }
+    }
+}
